1d6e983236f89c4bc042528c0266018d5319224f
qjzy2008 <qjzy2008@163.com>
2014-07-12 10:29:41 +0800

    check progress is aready running

--- start_watch_valgrind_log.sh
+++ start_watch_valgrind_log.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-python watch_valgrind_log.py &
+PID=`ps -ef |grep "python watch_valgrind_log.py" |grep -v grep | awk '{print $2}'`
+if [ "$PID" != "" ]; then
+echo "logwatcher is runing!"
+exit
+fi
+echo "python watch_valgrind_log.py &"
+#python watch_valgrind_log.py &
